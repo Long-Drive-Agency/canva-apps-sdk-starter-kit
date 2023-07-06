30ffb1913a76eca328e74558635234f4f40fc61c
Dylan Windebank <dylanwindebank@my.unt.edu>
2023-07-06 17:00:05 -0500

    attempted authroiuzation, to be resumed

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,20 @@
 import { Button, Rows, Text } from "@canva/app-ui-kit";
 import { addNativeElement } from "@canva/design";
+import { auth } from "@canva/user";
 import * as React from "react";
+import useEffect from "react";
 import styles from "styles/components.css";
 
 export const App = () => {
+  var result;
+  useEffect(() => {
+    async function drAuthentication(){
+      result = await auth.requestAuthentication();
+    }
+    drAuthentication();
+    console.log(result)
+  }, []);
+  
   const onClick = () => {
     addNativeElement({
       type: "TEXT",
@@ -15,12 +26,10 @@ export const App = () => {
     <div className={styles.scrollContainer}>
       <Rows spacing="2u">
         <Text>
-          To make changes to this app, edit the{" "}
-          <code className={styles.code}>src/app.tsx</code> file, then close and
-          reopen the app in the editor to preview the changes.
+          You're connected. Press the button below to access your design on other Long Drive applications.
         </Text>
         <Button variant="primary" onClick={onClick} stretch>
-          Do something cool
+          Export Design
         </Button>
       </Rows>
     </div>
